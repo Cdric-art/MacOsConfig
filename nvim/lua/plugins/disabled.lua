34b7489fa9d0338f69e7d58af8a48569e0afcdde
Cdric-art <cdric.art@gmail.com>
2024-07-29 14:33:22 +0200

    add new config nvim

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "garymjr/nvim-snippets",
+        enabled = false,
+    },
+    {
+        "rafamadriz/friendly-snippets",
+        enabled = false,
+    },
+    {
+        "tokyonight.nvim",
+        enabled = false,
+    },
+}
