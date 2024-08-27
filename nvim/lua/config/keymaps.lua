bc9cda8b202bbe5853b93dad7af2594b280601fc
Cdric-art <cdric.art@gmail.com>
2024-08-27 13:46:55 +0200

    Delete nvim config

--- nvim/lua/config/keymaps.lua
+++
@@ -1,7 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.keymap.set("n", "<D-s>", "<cmd>wa<CR>")
-vim.keymap.set("n", "<tab>", ":BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<leader>x", ":bdelete!<CR>")
